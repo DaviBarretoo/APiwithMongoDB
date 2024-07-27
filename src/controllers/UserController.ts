ffd884bddae8c234d926827c50b59920409d9ece
DaviBarretoo <davibarretojkda@gmail.com>
2024-07-27 13:21:14 -0300

    password cryptography

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -2,12 +2,40 @@ import { Request, Response } from "express";
 import User from "../models/User";
 import { UserLogin } from "../interfaces/interfaceforuser";
 import * as EmailValidator from "email-validator";
+import bcrypt from 'bcrypt'
+
+const saltRounds = 10 
+
+ const PasswordBcrypt = async (password: string ): Promise<string> => {
+  try{ 
+    const salt = await bcrypt.genSalt(saltRounds)
+    const hPassword = await bcrypt.hash(password,salt)
+    return hPassword
+  }catch(err) {
+    console.log(err)
+    throw new Error("Error")
+  }
+ }
+
+ const cPasswordBcrypt = async(password:string, PBcrypt: string): Promise<boolean> =>{
+  try{
+      return await bcrypt.compare(password, PBcrypt)
+  }catch(err){
+    console.log(err + "cPasswordBcrypt")
+    throw new Error("Error")
+  }
+ }
+
+
+ let users: UserLogin[] = []
 
 export const register = async (req: Request, res: Response) => {
+  
   const { name, email, password }: UserLogin = req.body;
-
   try {
-    const newUser = new User({ name, email, password });
+    const passwordNew = await PasswordBcrypt(password)
+    const newUser: UserLogin = new User({ name, email, password:passwordNew });
+   
     const userInDB = await User.find();
     const emailInDB = userInDB.map((User) => User.email);
 
@@ -29,15 +57,20 @@ export const register = async (req: Request, res: Response) => {
 
     //checking the existence of the user in the database (incluedes check emailInDb === newUser.email)
     if (emailInDB.includes(newUser.email)) {
+    
       res.status(201).json({
         message: `Este User  já existe ${email}`,
       });
       return;
     } else {
+  
       await newUser.save();
+    users.push(newUser)
+   
+     
 
       res.status(201).json({
-        message: `new user ${email}`,
+        message: ` new user ${email}`,
       });
       return;
     }
